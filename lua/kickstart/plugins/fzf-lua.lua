0b05d70302fb65747fb84c412cbb98625a4d5bf8
jordantrent <jordan.trent@gmail.com>
2025-06-17 21:53:33 +0100

    telescope -> fzf-lua

---
+++ lua/kickstart/plugins/fzf-lua.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    'ibhagwan/fzf-lua',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('fzf-lua').setup {}
+
+      vim.keymap.set('n', '<leader>sf', require('fzf-lua').files, { desc = '[S]earcg [F]iles' })
+      vim.keymap.set('n', '<leader>sg', require('fzf-lua').live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader><leader>', require('fzf-lua').buffers, { desc = '[ ] Find existing buffers' })
+
+      vim.keymap.set('n', '<leader>sn', function()
+        require('fzf-lua').files { cwd = vim.fn.stdpath 'config' }
+      end, { desc = '[S]earch [N]eovim files' })
+    end,
+  },
+}
